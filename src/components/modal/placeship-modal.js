cb0631424784671d98c56252859c60d506294d65
Yuji T <accmanager1990@gmail.com>
2022-02-06 10:41:44 +0900

    Refactor and use board component

--- src/components/modal/placeship-modal.js
+++ src/components/modal/placeship-modal.js
@@ -2,28 +2,12 @@ import Modal from './modal';
 import './placeship-modal.css';
 import Player from '../../js/player';
 import Gameboard from '../../js/gameboard';
+import BoardComponent from '../board/board-component';
 
 export default () => {
   const player = Player(Gameboard.create());
   const modal = Modal();
-  const cells = [];
-  const ships = (() => {
-    const shipTypes = [
-      'Carrier',
-      'BattleShip',
-      'Destroyer',
-      'Submarine',
-      'Patrol Boat',
-    ];
-    const shipSizes = [5, 4, 3, 3, 2];
-    let currentShipIndex = 0;
-    const nextShip = () => {
-      currentShipIndex += 1;
-    };
-    const getCurrentShipType = () => shipTypes[currentShipIndex];
-    const getCurrentShipSize = () => shipSizes[currentShipIndex];
-    return { nextShip, getCurrentShipSize, getCurrentShipType };
-  })();
+  let currentShipIndex = 0;
   let direction = Gameboard.HORIZONTAL;
   function changeDirection() {
     direction =
@@ -35,15 +19,14 @@ export default () => {
   const rotateBtn = document.createElement('button');
   const shipTypeContainer = document.createElement('div');
   const shipType = document.createElement('p');
-  const gridContainer = document.createElement('div');
+  const boardComponent = BoardComponent();
 
   modal.content.classList.add('psmodal-content-container');
   shipType.classList.add('ship-type');
-  gridContainer.classList.add('grid-container');
 
   modal.setTitle('Place your ships');
   rotateBtn.textContent = 'Rotate';
-  shipType.textContent = ships.getCurrentShipType();
+  shipType.textContent = Gameboard.SHIPS[currentShipIndex].type;
 
   modal.content.appendChild(rotateBtn);
   shipTypeContainer.appendChild(shipType);
@@ -71,18 +54,18 @@ export default () => {
         ) {
           if (addOrRemove === 'add') {
             if (i === 0) {
-              cells[mY][mX].classList.add(classStart);
+              boardComponent.cells[mY][mX].classList.add(classStart);
             } else if (i === size - 1) {
-              cells[mY][mX].classList.add(classEnd);
+              boardComponent.cells[mY][mX].classList.add(classEnd);
             }
-            cells[mY][mX].classList.add('hovered');
+            boardComponent.cells[mY][mX].classList.add('hovered');
           } else if (addOrRemove === 'remove') {
             if (i === 0) {
-              cells[mY][mX].classList.remove(classStart);
+              boardComponent.cells[mY][mX].classList.remove(classStart);
             } else if (i === size - 1) {
-              cells[mY][mX].classList.remove(classEnd);
+              boardComponent.cells[mY][mX].classList.remove(classEnd);
             }
-            cells[mY][mX].classList.remove('hovered');
+            boardComponent.cells[mY][mX].classList.remove('hovered');
           }
         }
       }
@@ -90,30 +73,34 @@ export default () => {
     function cellEnterListener(e) {
       const x = parseInt(e.target.getAttribute('x'), 10);
       const y = parseInt(e.target.getAttribute('y'), 10);
-      listenerTemplate(y, x, ships.getCurrentShipSize(), 'add');
+      listenerTemplate(y, x, Gameboard.SHIPS[currentShipIndex].size, 'add');
     }
 
     function cellLeaveListener(e) {
       const x = parseInt(e.target.getAttribute('x'), 10);
       const y = parseInt(e.target.getAttribute('y'), 10);
-      listenerTemplate(y, x, ships.getCurrentShipSize(), 'remove');
+      listenerTemplate(y, x, Gameboard.SHIPS[currentShipIndex].size, 'remove');
     }
 
     return { cellEnterListener, cellLeaveListener };
   })();
 
-  function cellClickListener(y, x) {
-    if (player.placeShip(y, x, ships.getCurrentShipSize(), direction)) {
-      ships.nextShip();
+  function cellClickListener(e) {
+    const x = parseInt(e.target.getAttribute('x'), 10);
+    const y = parseInt(e.target.getAttribute('y'), 10);
+    if (
+      player.placeShip(y, x, Gameboard.SHIPS[currentShipIndex].size, direction)
+    ) {
+      currentShipIndex += 1;
       render();
     }
   }
 
   function render() {
-    if (ships.getCurrentShipType()) {
+    if (Gameboard.SHIPS[currentShipIndex]) {
       // removing and appending to trigger css animation
       shipTypeContainer.removeChild(shipType);
-      shipType.textContent = ships.getCurrentShipType();
+      shipType.textContent = Gameboard.SHIPS[currentShipIndex].type;
       shipTypeContainer.appendChild(shipType);
     } else {
       modal.content.removeChild(shipTypeContainer);
@@ -123,28 +110,28 @@ export default () => {
     for (let y = 0; y < 10; y += 1) {
       for (let x = 0; x < 10; x += 1) {
         if (player.board[y][x] !== Gameboard.EMPTY) {
-          cells[y][x].classList.add('occupied');
+          boardComponent.cells[y][x].classList.add('occupied');
         }
         if (
           JSON.stringify(player.board[y][x].head) ===
           JSON.stringify([y, x, Gameboard.VERTICAL])
         ) {
-          cells[y][x].classList.add('v-ship-start');
+          boardComponent.cells[y][x].classList.add('v-ship-start');
         } else if (
           JSON.stringify(player.board[y][x].tail) ===
           JSON.stringify([y, x, Gameboard.VERTICAL])
         ) {
-          cells[y][x].classList.add('v-ship-end');
+          boardComponent.cells[y][x].classList.add('v-ship-end');
         } else if (
           JSON.stringify(player.board[y][x].head) ===
           JSON.stringify([y, x, Gameboard.HORIZONTAL])
         ) {
-          cells[y][x].classList.add('h-ship-start');
+          boardComponent.cells[y][x].classList.add('h-ship-start');
         } else if (
           JSON.stringify(player.board[y][x].tail) ===
           JSON.stringify([y, x, Gameboard.HORIZONTAL])
         ) {
-          cells[y][x].classList.add('h-ship-end');
+          boardComponent.cells[y][x].classList.add('h-ship-end');
         }
       }
     }
@@ -152,22 +139,21 @@ export default () => {
 
   (() => {
     for (let y = 0; y < 10; y += 1) {
-      cells.push(Array(10));
       for (let x = 0; x < 10; x += 1) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        gridContainer.appendChild(cell);
-        cell.setAttribute('x', x);
-        cell.setAttribute('y', y);
-        cell.addEventListener('mouseenter', cellEnterListener);
-        cell.addEventListener('mouseleave', cellLeaveListener);
-        cell.addEventListener('click', cellClickListener.bind(cell, y, x));
-        cells[y][x] = cell;
+        boardComponent.cells[y][x].addEventListener(
+          'mouseenter',
+          cellEnterListener
+        );
+        boardComponent.cells[y][x].addEventListener(
+          'mouseleave',
+          cellLeaveListener
+        );
+        boardComponent.cells[y][x].addEventListener('click', cellClickListener);
       }
     }
   })();
 
-  modal.content.appendChild(gridContainer);
+  modal.content.appendChild(boardComponent.container);
 
   return { ...modal };
 };
