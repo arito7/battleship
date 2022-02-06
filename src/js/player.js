4b8493e843b4e3eb1a2267337e187f025e4c2d34
Yuji T <accmanager1990@gmail.com>
2022-02-06 14:59:45 +0900

    Add random ship placement for AI

--- src/js/player.js
+++ src/js/player.js
@@ -1,9 +1,16 @@
-export default (gameboard) => {
-  gameboard.placeShip(2, 1, 5, 1);
-  gameboard.placeShip(0, 0, 4);
-  gameboard.placeShip(8, 7, 3);
-  gameboard.placeShip(4, 6, 3);
-  gameboard.placeShip(9, 2, 2);
+import Gameboard from './gameboard';
+export default (gameboard, bot = false) => {
+  if (bot) {
+    Gameboard.SHIPS.forEach((ship) => {
+      while (true) {
+        const x = Math.round(Math.random() * 9);
+        const y = Math.round(Math.random() * 9);
+        if (gameboard.placeShip(y, x, ship.size)) {
+          break;
+        }
+      }
+    });
+  }
 
   const attack = (y, x, player) => player.receiveAttack(y, x);
   return { attack, ...gameboard };
